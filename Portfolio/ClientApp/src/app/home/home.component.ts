151fed1cb16ef3ad8fa27d157bfd60484e4a1c48
Vincent Garavuso <vinnyg96@hotmail.com>
2020-09-01 19:41:11 -0400

    Upgraded to angular 10 and published app to azure.

---
+++ Portfolio/ClientApp/src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { GithubService } from '../../services/github.service';
+import { Owner } from '../../dataModels/github';
+import { Repo } from '../../dataModels/github';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+})
+
+export class HomeComponent implements OnInit {
+  public userRepos: Repo[] = [];
+  constructor(private githubService: GithubService) {
+
+  }
+  ngOnInit() {
+    this.githubService.getRepositories('VincentGaravuso')
+      .subscribe(repos => {
+        this.userRepos = repos;
+        console.log(repos);
+      });
+  }
+}
---
+++ Portfolio/ClientApp/src/services/github.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Repo } from '../dataModels/github';
+
+@Injectable()
+export class GithubService {
+
+  apiUrl = 'https://api.github.com/users';
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Get all users
+   */
+  getUsers() {
+    return this.http.get(this.apiUrl + '?per_page=10');
+  }
+  /**
+   * Get user by username
+   * @param username
+   */
+  getUser(username: string) {
+    return this.http.get(this.apiUrl + '/${username}');
+  }
+  /**
+   * Get repositories of user
+   * @param username
+   */
+  getRepositories(username: string) {
+    return this.http.get<Repo[]>(this.apiUrl + '/' + username + '/repos');
+  }
+}
